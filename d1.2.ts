329f9638ca15a60ec5d86fcb91cd7f69bf805991
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2024-12-02 11:48:33 +0100

    remove redundant map

--- d1.2.ts
+++ d1.2.ts
@@ -9,17 +9,17 @@ function run() {
     const lines = text.split("\r\n");
 
     const list: number[] = Array.from([]);
-    const counts = Array.from([new Map<number, number>(), new Map<number, number>()]);
+    const counts = new Map<number, number>();
 
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i].split("   ").map(i => parseInt(i));
         list.push(line[0]);
-        counts[1].set(line[1], (counts[1].get(line[1]) ?? 0) + 1);
+        counts.set(line[1], (counts.get(line[1]) ?? 0) + 1);
     }
 
     let sum = 0;
     for (let i = 0; i < lines.length; i++) {
-        sum += list[i] * (counts[1].get(list[i]) ?? 0);
+        sum += list[i] * (counts.get(list[i]) ?? 0);
     }
     return {
         result: sum,
