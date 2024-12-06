4470df84b8661b66887a68e93e1fc72ba18634ab
alexa <alex@targoninc.com>
2024-12-07 00:22:18 +0100

    optimize d5 p2

--- d5/d5.2.ts
+++ d5/d5.2.ts
@@ -5,17 +5,17 @@ const input = Bun.file(inputFile);
 const text = await input.text();
 
 function run() {
-    const start = performance.now();
+    const start = Bun.nanoseconds();
     const lines = text.split("\r\n");
     const emptyLineIndex = lines.indexOf("");
     const ruleMap = new Map<number, Map<number, boolean>>();
-    lines.slice(0, emptyLineIndex).map(l => {
-        const rule = l.split("|").map(sn => parseInt(sn));
+    for (let i = emptyLineIndex; i < lines.length; i++) {
+        const rule = lines[i].split("|").map(sn => parseInt(sn));
         if (!ruleMap.has(rule[0])) {
             ruleMap.set(rule[0], new Map<number, boolean>());
         }
         ruleMap.get(rule[0])!.set(rule[1], true);
-    });
+    }
     const updates = lines.slice(emptyLineIndex + 1);
     let sum = 0;
 
@@ -46,7 +46,7 @@ function run() {
         }
     }
 
-    const diff = performance.now() - start;
+    const diff = Bun.nanoseconds() - start;
     return {
         result: sum,
         time: diff
@@ -56,12 +56,12 @@ function run() {
 console.log(script.replace(".txt", "").split("\\").at(-1));
 const { result, time } = run();
 console.log(`Result: ${result}`);
-console.log("Measuring...");
 
+console.log("Measuring...");
 let sumTime = 0;
 const runCount = 1000;
 for (let i = 0; i < runCount; i++) {
     const runTime = run().time;
     sumTime += runTime;
 }
-console.log((sumTime / runCount).toFixed(2) + "ms per run");
+console.log((sumTime / runCount / 1000 / 1000).toFixed(2) + "ms per run");
