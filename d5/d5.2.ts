c7c041a509551c024e2b4066bb67d6ee23f45702
alexa <alex@targoninc.com>
2024-12-07 00:07:41 +0100

    optimize d5 p2 :3

--- d5/d5.2.ts
+++ d5/d5.2.ts
@@ -8,45 +8,42 @@ function run() {
     const start = performance.now();
     const lines = text.split("\r\n");
     const emptyLineIndex = lines.indexOf("");
-    const rules = lines.slice(0, emptyLineIndex).map(l => l.split("|").map(sn => parseInt(sn)));
+    const ruleMap = new Map<number, Map<number, boolean>>();
+    lines.slice(0, emptyLineIndex).map(l => {
+        const rule = l.split("|").map(sn => parseInt(sn));
+        if (!ruleMap.has(rule[0])) {
+            ruleMap.set(rule[0], new Map<number, boolean>());
+        }
+        ruleMap.get(rule[0])!.set(rule[1], true);
+    });
     const updates = lines.slice(emptyLineIndex + 1);
     let sum = 0;
-    const badUpdates = [];
 
-    updates: for (const update of updates) {
+    for (const update of updates) {
         const numbers = update.split(",").map(n => parseInt(n));
-        const relevantRules = rules.filter(r => r.every(rn => numbers.includes(rn)));
+        let isBad = false;
 
-        for (const rule of relevantRules) {
-            const aIndex = numbers.indexOf(rule[0]);
-            const bIndex = numbers.indexOf(rule[1]);
-            if (bIndex < aIndex) {
-                badUpdates.push({relevantRules, numbers});
-                continue updates;
-            }
-        }
-    }
-
-    for (const {relevantRules, numbers} of badUpdates) {
         for (let i = 0; i < numbers.length; i++) {
             let swapped = true, hasSwapped = false;
             while (swapped) {
                 swapped = false;
-                const rule = relevantRules.find(rn => rn[1] === numbers[i] && rn[0] === numbers[i + 1]);
-                if (rule) {
-                    numbers[i] = rule[0];
-                    numbers[i + 1] = rule[1];
+                if (ruleMap.get(numbers[i + 1])?.has(numbers[i])) {
+                    const cache = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = cache;
                     swapped = true;
                     hasSwapped = true;
+                    isBad = true;
                 }
             }
             if (hasSwapped) {
                 i = -1;
             }
         }
-
-        const middle = numbers[Math.floor(numbers.length / 2)];
-        sum += middle;
+        if (isBad) {
+            const middle = numbers[Math.floor(numbers.length / 2)];
+            sum += middle;
+        }
     }
 
     const diff = performance.now() - start;
@@ -60,7 +57,7 @@ console.log(script.replace(".txt", "").split("\\").at(-1));
 const { result, time } = run();
 console.log(`Result: ${result}`);
 console.log("Measuring...");
-/*
+
 let sumTime = 0;
 const runCount = 1000;
 for (let i = 0; i < runCount; i++) {
@@ -68,4 +65,3 @@ for (let i = 0; i < runCount; i++) {
     sumTime += runTime;
 }
 console.log((sumTime / runCount).toFixed(2) + "ms per run");
-*/
